dff23be9232fc62102b7c8be982aa4e47cb3873e
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-05-19 23:50:07 +0530

    Color changes

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -23,7 +23,6 @@ export default function Contact() {
                     </div>
                 </div>
                 <div className="contact-image-div">
-                    <img alt="Saad Working" src={require("../../assests/images/contactMail.png")}></img>
                 </div>
             </div>
         </div>
