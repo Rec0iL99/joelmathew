7863f8a0e90d8ea2087f97ec3d04b297571dd30e
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-07-09 03:31:17 +0530

    Updates

---
+++ precache-manifest.9b1425b5c94f29e54ebdabdfd1fd68d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c817a593460222ff6afc1df16d4e7f8d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d26acc9ad29d7d4ea8f",
+    "url": "./static/css/main.1fe6c217.chunk.css"
+  },
+  {
+    "revision": "2b2b9d3e9cce8241a1eb",
+    "url": "./static/js/2.04c555af.chunk.js"
+  },
+  {
+    "revision": "5d26acc9ad29d7d4ea8f",
+    "url": "./static/js/main.07229903.chunk.js"
+  },
+  {
+    "revision": "74c370ab0236dd260323",
+    "url": "./static/js/runtime-main.285e7092.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "./static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "./static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "02470dbc6d12dbbcb17ef28aacd9bb39",
+    "url": "./static/media/manOnTable.02470dbc.svg"
+  }
+]);
